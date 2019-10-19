8dd6d4543e6efc255a9d7744afa29f6fa115ed65
Luz <yagao0o@126.com>
2019-10-19 18:51:33 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
 var Base16 = null;
-if (process.versions.node >= "6.4.0") {
-    Base16 = require("./src/Base16");
-}
-else {
-    Base16 = require("./lib/Base16");
-}
+Base16 = require("./lib/Base16");
 module.exports = Base16;
